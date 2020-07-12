Fixed benchmark function names. Changed normalization after convolution to Clip-Clip algorithm. Generated test images for different normalization algorithms. Changed C++ tests to use Clip-Clip test data.

scripts/generate_test_data.py
@@ -1,8 +1,11 @@
+import os.path
+
 import numpy as np
-from PIL import Image
 import torch
 import torch.nn.functional as F
-import os.path
+from PIL import Image
+from itertools import product
+from tqdm import tqdm
 
 ASYMMETRIC_REFLECT = "ASYMMETRIC_REFLECT"
 AVOID = "AVOID"
@@ -12,7 +15,7 @@ REPEAT = "REPEAT"
 SYMMETRIC_REFLECT = "SYMMETRIC_REFLECT"
 WRAP = "WRAP"
 
-methods = (
+border_treatment_methods = (
     ASYMMETRIC_REFLECT,
     AVOID,
     CONSTANT_0,
@@ -23,29 +26,29 @@ methods = (
 )
 
 
-def prepare_input(arr, method, padding):
+def prepare_input(arr, border_treatment_method, padding):
     padding_y, padding_x = padding
     if len(arr.shape) == 3:
         padding = ((padding_y, padding_y), (padding_x, padding_x), (0, 0))
     else:
         padding = ((padding_y, padding_y), (padding_x, padding_x))
 
-    if method == "ASYMMETRIC_REFLECT":
+    if border_treatment_method == "ASYMMETRIC_REFLECT":
         return np.pad(arr, padding, 'reflect')
-    elif method == "AVOID":
+    elif border_treatment_method == "AVOID":
         return np.array(arr)
-    elif method == "CONSTANT_0":
+    elif border_treatment_method == "CONSTANT_0":
         return np.pad(arr, padding, constant_values=0)
-    elif method == "CONSTANT_2":
+    elif border_treatment_method == "CONSTANT_2":
         return np.pad(arr, padding, constant_values=2)
-    elif method == "REPEAT":
+    elif border_treatment_method == "REPEAT":
         return np.pad(arr, padding, 'edge')
-    elif method == "SYMMETRIC_REFLECT":
+    elif border_treatment_method == "SYMMETRIC_REFLECT":
         return np.pad(arr, padding, 'symmetric')
-    elif method == "WRAP":
+    elif border_treatment_method == "WRAP":
         return np.pad(arr, padding, 'wrap')
     else:
-        raise ValueError(f"Unknown method '{method}'.")
+        raise ValueError(f"Unknown method '{border_treatment_method}'.")
 
 
 def print_array(arr):
@@ -64,7 +67,7 @@ def print_array(arr):
 
 
 def read_image(file_name, file_extension):
-    image = Image.open(f'../resources/tests/{file_name}.{file_extension}')
+    image = Image.open(f'./resources/tests/{file_name}.{file_extension}')
     image = np.asarray(image)
     image = image.astype(np.float64) / np.max(image)
     if len(image.shape) == 2:
@@ -80,7 +83,7 @@ def get_correct_filter(channels):
     ]
     kernel = [
         [-1, -1, -1],
-        [-1, 9, -1],
+        [-1, 8, -1],
         [-1, -1, -1]
     ]
 
@@ -111,16 +114,27 @@ def convolve(image, stride, dilation):
     return result_data
 
 
-def normalize_after_convolution(arr):
-    arr = arr - np.min(arr)
-    arr = arr / np.max(arr)
+def normalize_after_convolution(arr, mode):
+    if mode == "rr":
+        arr -= np.min(arr)
+        arr /= np.max(arr)
+    elif mode == "cr":
+        arr_max = np.max(arr)
+        arr = np.clip(arr, a_min=0, a_max=arr_max)
+        arr /= arr_max
+    elif mode == "cc":
+        arr = np.clip(arr, a_min=0, a_max=1.0)
+    else:
+        raise ValueError(f"Unknown normalization mode '{mode}'")
 
     return np.round(arr * 255, 11)
 
 
-def save_image(arr, file_name, method, file_extension, padding, stride, dilation, format):
+def save_image(arr, file_name, border_treatment_method, normalization_method, file_extension, options, file_format):
+    padding, stride, dilation = options
+
     option_folder = f'{"x".join(map(str, padding))}-{"x".join(map(str, stride))}-{"x".join(map(str, dilation))}'
-    file_path = f'../resources/tests/{option_folder}/{method}/{file_name}.{file_extension}'
+    file_path = f'./resources/tests/{normalization_method}/{option_folder}/{border_treatment_method}/{file_name}.{file_extension}'
     arr = arr.astype(np.uint8)
 
     if len(arr.shape) == 2:
@@ -133,22 +147,22 @@ def save_image(arr, file_name, method, file_extension, padding, stride, dilation
         os.makedirs(directory)
     if file_extension == "pbm":
         result_image = result_image.convert('1')
-    result_image.save(file_path, format=format)
+    result_image.save(file_path, format=file_format)
 
 
-def convolve_with_method(options, method, file_extension, file_format):
+def convolve_with_method(options, method, normalization_method, file_extension, file_format):
     file_name = "Piercing-The-Ocean"
     image = read_image(file_name, file_extension)
-
     padding, stride, dilation = options
 
     prepared_image = prepare_input(image, method, padding)
     result_data = convolve(prepared_image, stride, dilation)
 
-    result_data = normalize_after_convolution(result_data)
+    result_data = normalize_after_convolution(result_data, normalization_method)
     if result_data.shape[2] == 1:
         result_data = result_data.squeeze(2)
-    save_image(result_data, file_name, method, file_extension, padding, stride, dilation, file_format)
+
+    save_image(result_data, file_name, method, normalization_method, file_extension, options, file_format)
 
 
 def main():
@@ -165,10 +179,17 @@ def main():
         ("ppm", "PPM"),
     )
 
-    for options in list_of_options:
-        for method in methods:
-            for file_extension, file_format in extensions:
-                convolve_with_method(options, method, file_extension, file_format)
+    normalization_methods = (
+        "rr",
+        "cr",
+        "cc",
+    )
+
+    parameters = (list_of_options, border_treatment_methods, normalization_methods, extensions)
+    total = np.prod([len(x) for x in parameters])
+
+    for options, method, normalization_method, (file_extension, file_format) in tqdm(product(*parameters), ncols=100, total=total):
+        convolve_with_method(options, method, normalization_method, file_extension, file_format)
 
 
 if __name__ == "__main__":