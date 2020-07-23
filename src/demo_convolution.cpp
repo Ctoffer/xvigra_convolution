5b1479ca1d87734228208f4e1177ff7df3a9c2ef
Christopher Schuster <schustrchr@gmail.com>
2020-07-24 01:43:26 +0200

    Updated demo with black-and-white & color images. Enabled complete build again

--- src/demo_convolution.cpp
+++ src/demo_convolution.cpp
@@ -1,4 +1,6 @@
+#include <array>
 #include <stdexcept>
+#include <string>
 
 #include "xtensor/xtensor.hpp"
 #include "xtensor/xio.hpp"
@@ -7,34 +9,36 @@
 #include "xvigra/image_io.hpp"
 
 
-void runGaussianSmoothingDemo() {
+void runGaussianSmoothingDemo(const std::string& fileExtension) {
     xt::xtensor<float, 3> image = xvigra::loadImageAsXTensor<float>(
-        "./resources/src/Piercing-The-Ocean.pgm"
+        "./resources/src/Piercing-The-Ocean." + fileExtension
     );
     auto result = xvigra::gaussianSmoothing<2>(image, std::array<double, 2>{1.5, 1.5});
     xvigra::saveImage(
-        "./resources/src/demo_gaussian_smoothing.pgm",
+        "./resources/src/demo_gaussian_smoothing." + fileExtension,
          xvigra::normalizeAfterConvolution<float>(result)
     );
 }
 
 
-void runGaussianSharpeningDemo() {
+void runGaussianSharpeningDemo(const std::string& fileExtension) {
     xt::xtensor<float, 3> image = xvigra::loadImageAsXTensor<float>(
-        "./resources/src/Piercing-The-Ocean.pgm"
+        "./resources/src/Piercing-The-Ocean." + fileExtension
     );
     auto result = xvigra::gaussianSharpening<2>(image, 0.4, 2.5);
     auto res = xvigra::normalizeAfterConvolution<float>(result);
     xvigra::saveImage(
-        "./resources/src/demo_gaussian_sharpening.pgm",
+        "./resources/src/demo_gaussian_sharpening." + fileExtension,
         res
     );
 }
 
 
 int main() {
-    runGaussianSmoothingDemo();
-    runGaussianSharpeningDemo();
+    for (const auto& fileExtension : std::array<std::string, 2>{"pgm", "ppm"}) {
+        runGaussianSmoothingDemo(fileExtension);
+        runGaussianSharpeningDemo(fileExtension);
+    }
 
     return 0;
 }
